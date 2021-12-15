3b44caa561caaea85396d9b942eae0959e818253
quocdat2k <hellodat1401@gmail.com>
2021-12-14 22:06:55 -0800

    First_commit_NguyenQuocDat017

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NguyenQuocDat017.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
