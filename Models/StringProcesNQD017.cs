08b6f5435ada65265cc908b462a5069f8cc746f5
quocdat2k <hellodat1401@gmail.com>
2021-12-14 23:14:04 -0800

    Xong c5

---
+++ Models/StringProcesNQD017.cs
@@ -0,0 +1,11 @@
+namespace NguyenQuocDat017.Models
+{
+    public class StringProcessNQD017
+    {
+        public string LowerToUpper(string strInput)
+        {
+            return strInput.ToUpper();
+        }
+
+    }
+}
